ec04245c30470f29b90c6359aeb719b6e0f9fa94
Thomas Gollenia <thomas@gollenia.at>
2024-04-26 10:14:41 +0200

    Refactor Radio component

--- src/Fields/Radio.tsx
+++ src/Fields/Radio.tsx
@@ -6,53 +6,59 @@ export type RadioProps = {
 	name: string;
 	required: boolean;
 	width: number;
-	options?: Array< string >;
+	options?: Array<string>;
 	disabled: boolean;
-	onChange: ( value: string ) => void;
+	onChange: (value: string) => void;
 };
 
-const Radio = ( props: RadioProps ) => {
-	const { onChange, options, name, disabled, placeholder, width } = props;
+const Radio = (props: RadioProps) => {
+	const { onChange, options, name, disabled, placeholder, width, required } =
+		props;
 
 	const classes = [
 		'ctx-form-field',
 		'radio',
 		'input--width-' + width,
 		props.required ? 'select--required' : '',
-	].join( ' ' );
+	].join(' ');
 
-	const [ selection, setSelection ] = useState( placeholder );
+	const [selection, setSelection] = useState(placeholder);
 
-	const onChangeHandler = ( event: any ) => {
-		setSelection( event.target.value );
-		onChange( event.target.value );
+	const onChangeHandler = (event: any) => {
+		setSelection(event.target.value);
+		onChange(event.target.value);
 	};
 
+	const firstOptionInitiallyChecked = required && !selection;
+
 	return (
-		<div className={classes} style={{
-			gridColumn: `span ${width}`
-		}}>
-			<fieldset name={ props.name }>
-				<legend>{ props.label }</legend>
-				{ options &&
-					options.map( ( option, index ) => {
+		<div
+			className={classes}
+			style={{
+				gridColumn: `span ${width}`,
+			}}
+		>
+			<fieldset name={props.name}>
+				<legend>{props.label}</legend>
+				{options &&
+					options.map((option, index) => {
 						return (
-							<label key={ index } htmlFor={ name + index }>
+							<label key={index} htmlFor={name + index}>
 								<input
-									checked={ selection === option }
-									onChange={ ( value ) => {
-										onChangeHandler( value );
-									} }
-									disabled={ disabled }
+									checked={selection === option}
+									onChange={(value) => {
+										onChangeHandler(value);
+									}}
+									disabled={disabled}
 									type="radio"
-									value={ option }
-									name={ name }
-									id={ name + index }
+									value={option}
+									name={name}
+									id={name + index}
 								/>
-								{ option }
+								{option}
 							</label>
 						);
-					} ) }
+					})}
 			</fieldset>
 		</div>
 	);
