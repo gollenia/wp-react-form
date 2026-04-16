e36c1fe63f2bce8e66c8d2d0bddeddd90a4ae9cd
Thomas Gollenia <thomas@gollenia.at>
2026-04-16 19:21:56 +0200

    exports updated

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,73 @@
-import Checkbox from './Fields/Checkbox';
-import Combobox from './Fields/Combobox';
-import Country from './Fields/Country';
-import Hidden from './Fields/Hidden';
-import HtmlBlock from './Fields/HtmlBlock';
-import Input from './Fields/Input';
-import Radio from './Fields/Radio';
-import Select from './Fields/Select';
-import Submit from './Fields/Submit';
-import TextArea from './Fields/TextArea';
-import InputField from './InputField';
+import Button from './components/Button/Button';
+import Checkbox from './components/Fields/Checkbox/Checkbox';
+import Combobox from './components/Fields/Combobox/Combobox';
+import Country from './components/Fields/Country/Country';
+import Hidden from './components/Fields/Hidden/Hidden';
+import Input from './components/Fields/Input/Input';
+import Radio from './components/Fields/Radio/Radio';
+import Range from './components/Fields/Range/Range';
+import Select from './components/Fields/Select/Select';
+import { Stepper } from './components/Fields/Stepper/Stepper';
+import Submit from './components/Fields/Submit/Submit';
+import TextArea from './components/Fields/TextArea/TextArea';
+import { Flex } from './components/Flex/Flex';
+import Form from './components/Form/Form';
+import { Fieldset } from './components/FormFields/FormFields';
+import FormField from './components/InputField/InputField';
 
-import Form from './Form';
-
-export { Checkbox, Combobox, Country, Hidden, HtmlBlock, Input, InputField, Radio, Select, Submit, TextArea };
+export type {
+	FieldRenderProps,
+	FieldValue,
+	FormFieldDefinition,
+	FormState,
+	FormValues,
+	InputType,
+	VisibilityRule,
+	CountryRegion,
+	FormResponse,
+	SelectOptions,
+} from './types';
+export type { CheckboxProps, CheckboxVariant } from './components/Fields/Checkbox/Checkbox';
+export type { ComboboxProps } from './components/Fields/Combobox/Combobox';
+export type { CountryProps } from './components/Fields/Country/Country';
+export type { InputFieldTypes, InputProps } from './components/Fields/Input/Input';
+export type { RadioProps } from './components/Fields/Radio/Radio';
+export type { RangeInputProps } from './components/Fields/Range/Range';
+export type { SelectProps } from './components/Fields/Select/Select';
+export type { TextAreaProps } from './components/Fields/TextArea/TextArea';
+export {
+	getDefaultFormValues,
+	normalizeFieldValue,
+} from './modules/values';
+export { isFieldVisible } from './modules/visibility';
+export type { CountryOption } from './modules/countries';
+export { getCountryOptions } from './modules/countries';
+export type {
+	FlexAlign,
+	FlexDirection,
+	FlexJustify,
+	FlexProps,
+	FlexWrap,
+} from './components/Flex/Flex';
+export { sanitizeHtml, sanitizeInlineHtml } from './modules/sanitize';
+export {
+	Button,
+	Checkbox,
+	Combobox,
+	Country,
+	Fieldset,
+	Flex,
+	FormField,
+	Form,
+	Hidden,
+	Input,
+	FormField as InputField,
+	Fieldset as FormFields,
+	Radio,
+	Range,
+	Select,
+	Stepper,
+	Submit,
+	TextArea,
+};
 export default Form;
-
