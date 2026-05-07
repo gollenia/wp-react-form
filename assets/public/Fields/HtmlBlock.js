554b70d92da3898aae52b2ea47a21c791de368b3
Thomas Gollenia <thomas@gollenia.at>
2026-05-07 09:18:31 +0200

    remove old asset folder

--- assets/public/Fields/HtmlBlock.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const HtmlBlock = (props) => {
-    const { content, width } = props;
-    const classes = ['core-block', 'grid__column--span-' + width].join(' ');
-    return (_jsx("div", { className: classes, dangerouslySetInnerHTML: { __html: content } }));
-};
-export default HtmlBlock;
--- assets/public/index.js
+++
@@ -1,4 +0,0 @@
-import InputField from './InputField';
-import Checkbox from './Fields/Checkbox';
-import './style.scss';
-export { Checkbox, InputField };
